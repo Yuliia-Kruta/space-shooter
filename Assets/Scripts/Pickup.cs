01c457e5956acda4ea24e03e365052d653e9ddf5
Yuliia Kruta <130898660+Yuliia-Kruta@users.noreply.github.com>
2025-04-27 23:15:07 +1000

    Create Heal Pickup

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -11,6 +11,8 @@ public class Pickup : MonoBehaviour
 {
     [SerializeField]
     public WeaponType weaponType;
+    [SerializeField]
+    private int healAmount = 10;
 
     void OnTriggerEnter2D(Collider2D col)
     {
@@ -44,9 +46,15 @@ public class Pickup : MonoBehaviour
         if (playerInput == null) {
             Debug.LogError("Player doesn't have a PlayerInput component.");
             return;
-        } else {
+        } 
+        if (CompareTag("Pickup")) // the triple shot pickup
+        {
             // tell the playerInput to SwapWeapon based on our weaponType
             playerInput.SwapWeapon(weaponType);
+        }
+        else if (CompareTag("Heal"))// any other pickup (the health)
+        {
+            player.GetComponent<IHealth>().Heal(healAmount);
         }
     }
 
